d48623e7042f2e8c80ee428ae90e94818d68f9f7
DavidHotaran <hotaran.d98@gmail.com>
2022-08-18 21:28:09 -0400

    split page in 2 for comps & layouts

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,40 @@
 export default function Home() {
   return (
     <div className='bg-slate-700 text-white h-screen'>
-      <h1 className='text-center pt-6 text-3xl underline font-bold'>Layouts</h1>
-      <div className='flex justify-center mt-4'>
-        <div className='grid grid-cols-6 gap-6 mt-5'>
-          <Card
-            href={"/dashboard"}
-            name={"Dashboard.js"}
-          />
+      <div className="grid grid-cols-2 h-full">
+        <div className="border-r-2">
+          <h1 className='text-center pt-6 text-3xl underline font-bold'>Layouts</h1>
+          <div className='flex justify-center mt-4'>
+            <div className='grid grid-cols-3 gap-6 mt-5'>
+              <Card
+                href={"/dashboard"}
+                name={"Dashboard.js"}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="border-l-2">
+          <h1 className='text-center pt-6 text-3xl underline font-bold'>Components</h1>
+          <div className='flex justify-center mt-4'>
+            <div className='grid grid-cols-3 gap-6 mt-5'>
+              <Card
+                href={"/components"}
+                name={"Button"}
+              />
+              <Card
+                href={"/components"}
+                name={"Footer"}
+              />
+              <Card
+                href={"/components"}
+                name={"Input"}
+              />
+              <Card
+                href={"/components/"}
+                name={"Navbar"}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -16,7 +43,7 @@ export default function Home() {
 
 function Card({ href, name }) {
   return (
-    <div className='w-40 h-40 border-4 rounded flex items-center justify-center cursor-pointer hover:bg-slate-600'
+    <div className='w-40 h-40 border-4 rounded flex items-center justify-center cursor-pointer hover:bg-slate-600 hover:text-blue-400'
       onClick={() => window.open(href, "_blank", "noreferrer")}
     >
       {name}
