d5d41a0a2fc20a73cb3b26bd15e484cddf44a842
DavidHotaran <hotaran.d98@gmail.com>
2022-08-18 21:28:49 -0400

    WIP for scrolling to element

--- pages/components.js
+++ pages/components.js
@@ -1,11 +1,20 @@
 import Button from "../components/Button";
 import Input from "../components/Input";
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
+import { useRouter } from 'next/router'
 
 export default function Components() {
     const [inputVal, setInputVal] = useState("");
+    const router = useRouter();
+    const ref = useRef(null);
+    const idToScrollTo = router.asPath.split("#")[1];
+
+    useEffect(() => {
+        if (ref.current) ref.current.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" })
+    }, []);
+
     return (
-        <div className="flex justify-center h-screen border-4 bg-slate-700 text-white">
+        <div className="flex justify-center min-h-screen h-full border-4 bg-slate-700 text-white">
             <div className="mt-4 flex flex-col /*w-full*/ align-middle">
                 <Container><Button variant={"primary"} text="primary" /></Container>
                 <Container><Button variant={"danger"} text="danger" onClick={() => alert("Danger variant")} /></Container>
@@ -16,6 +25,7 @@ export default function Components() {
                         placeholder="Search..."
                     />
                 </Container>
+                {/* {Array(100).fill("").map((_, i) => <div ref={i === idToScrollTo ? ref : null} id={i} key={i}>TEST</div>)} */}
             </div>
         </div>
     );
@@ -26,5 +36,5 @@ function Container({ children }) {
         <div className="mt-4">
             {children}
         </div>
-    )
-}
+    );
+};
